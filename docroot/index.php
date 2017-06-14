00cca019859391826bd129d90d36b4b9649cdb27
Philippe Milot <pmilot@turbulent.ca>
2017-06-14 09:03:26 -0400

    Fix require path

--- docroot/index.php
+++ docroot/index.php
@@ -1,6 +1,6 @@
 <?php
 
-require dirname(__DIR__).PATH_SEPARATOR."vendor".PATH_SEPARATOR."autoload.php";
+require dirname(__DIR__).DIRECTORY_SEPARATOR."vendor".DIRECTORY_SEPARATOR."autoload.php";
 
 (new %hatch_namespace%\Hello())->printHello();
 
